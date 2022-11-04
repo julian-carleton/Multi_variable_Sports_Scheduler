fc1a624acf2e7630f0bdb885ff70c07fc3d302d3
Julian <julianobandovelez@cmail.carleton.ca>
2022-11-04 17:21:44 -0400

    Included 1st tentative to team matchups using RR

--- MultiVariable_Sports_Scheduler/src/Scheduler/Schedule.java
+++ MultiVariable_Sports_Scheduler/src/Scheduler/Schedule.java
@@ -1,10 +1,53 @@
 package Scheduler;
 
 import java.util.ArrayList;
+import Scheduler.Round;
 
 public class Schedule {
-	ArrayList<TimeSlot> games;
+	ArrayList<Round> rounds;   
+	ArrayList<Object> teams;
+	int num_rounds;
 	
+	/** 
+	 * Default constructor
+	 * */
+	public Schedule(ArrayList<Object> teams) {
+		this.teams = teams;
+		this.rounds = new ArrayList<Round>();
+	}
 	
+	/**
+	 * 
+	 * 
+	 * */
+	public void make_matchups() {
+		
+	}
 	
+	/**
+	 * 
+	 * 
+	 * */
+	public void match_RR() {
+		//Determine if even or odd # teams
+		int num_teams = this.teams.size();
+		if (num_teams % 2 == 0) {
+			num_rounds = num_teams - 1;
+		} else {
+			num_rounds = num_teams;
+		}
+		
+		//Matching a round
+		int curr_x = 1;
+		int curr_y = num_teams;
+		
+		for (int i = 1; i < Math.floorDiv(num_teams, 2); i++) {
+			
+		}
+		
+	}
+	
+	public void match_round() {
+		
+	}
 }
