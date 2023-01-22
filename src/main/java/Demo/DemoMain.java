6a3c30f2c89ad7ad6240160a37013d39554b0291
Brady Norton <bnort.11@gmail.com>
2023-01-21 19:32:26 -0500

    Updated Demo class:
    - Functionality the same as League class main()
    
    Signed-off-by: Brady Norton <bnort.11@gmail.com>

--- src/main/java/Demo/DemoMain.java
+++ src/main/java/Demo/DemoMain.java
@@ -1,4 +1,33 @@
 package Demo;
 
+import Excel_Import_Export.CreateDataStrucs;
+import Excel_Import_Export.ExcelImport;
+import Scheduler.League;
+import Scheduler.Schedule;
+
 public class DemoMain {
+    public static void main(String[] args) {
+        ExcelImport excelImport = new ExcelImport();
+
+        /*
+         * Import sheets
+         */
+        excelImport.importData();
+
+        /*
+         * Create Data Types
+         */
+        CreateDataStrucs strucs = new CreateDataStrucs(excelImport.getTeams(), excelImport.getTimeExceptions(),excelImport.getDateExceptions(), excelImport.getArenas(),excelImport.getTimeSlots(),excelImport.getHomeArenas());
+
+
+        League league = new League("League", strucs.getDivisions(),strucs.getTimeslots(), strucs.getArenas());
+        league.generateSchedules();
+
+        for (Schedule s: league.getSchedules()) {
+            if (!s.getTimeSlots().isEmpty()) {
+                s.createSchedule();
+            }
+        }
+        System.out.print(false);
+    }
 }
